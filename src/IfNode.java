26310582fba33bb844ae96e2d959ed69238c0796
Samuel Kolb <samuel.kolb@me.com>
2014-11-13 22:39:48 +0100

    Initial commit

---
+++ src/IfNode.java
@@ -0,0 +1,19 @@
+/**
+ * Created by samuelkolb on 13/11/14.
+ *
+ * @author Samuel Kolb
+ */
+public class IfNode {
+
+	//region Variables
+
+	//endregion
+
+	//region Construction
+
+	//endregion
+
+	//region Public methods
+
+	//endregion
+}
